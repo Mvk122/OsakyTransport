70de2d8c80d956e1675732eae8cb23625f7d3c35
Madhav Khandhar <madhavkhandhar@gmail.com>
2021-04-30 12:20:31 +0800

    First commit and setup

---
+++ src/theme.js
@@ -0,0 +1,31 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#652D90'
+        },
+        background: {
+            default: "#ffffff"
+        },
+        secondary: {
+            main: "#00A551"
+        },
+        tertiary: {
+            main: "#00ADEE"
+        }
+    },
+    typography: {
+        "fontFamily": `"Roboto", "Helvetica", "Arial", sans-serif`,
+        "fontSize": 14,
+        "fontWeightLight": 300,
+        "fontWeightRegular": 400,
+        "fontWeightMedium": 500,
+        allVariants : {
+            color: "#652D90" 
+        },
+    },
+}
+)
+
+export default theme;
---
+++ src/views/Frontpage.js
@@ -0,0 +1,9 @@
+const Frontpage = props => {
+    return (
+        <div>
+            <h1>Hello world</h1>
+        </div>
+    )
+}
+
+export default Frontpage;
